ce833f31e369deb5396996a74f66d8c950bcb11e
Oderhohwo Tive Jude <tiveno4real@gmail.com>
2024-10-11 14:21:42 +0100

    menu home effect

---
+++ index.js
@@ -0,0 +1,7 @@
+let toggle = document.querySelector(".toggle");
+let menu = document.querySelector(".menu");
+
+toggle.addEventListener("click", function () {
+  menu.classList.toggle("active");
+  console.log("sdfbg");
+});
